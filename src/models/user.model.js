76097e0c8c0e6ee0ff397239e1cdd35519126e3a
MayankAgrawal <mayankagrawal0597@gmail.com>
2024-11-01 16:40:09 +0530

    add register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
-  userName:{
+  username:{
     type:String,
     required:true,
     unique:true,
@@ -50,7 +50,7 @@ userSchema.pre("save", async function (next) {
   if(!this.isModified("password")){
     return next();
   }
-  this.password = bcrypt.hash(this.password, 10)
+  this.password = await bcrypt.hash(this.password, 10)
   next()
 })
 
